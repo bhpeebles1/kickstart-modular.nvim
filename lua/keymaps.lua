aafec3bc9527d7dadc2e1851deacad6255e059e0
blakebluearrow <blake@bluearrowmail.com>
2024-05-05 16:15:22 -0400

    Added useful keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -11,6 +11,49 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagn
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
+-- Enter current file directory
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+
+-- Move selected text up and down
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
+-- Keep cursor position while moving lines upwards
+vim.keymap.set('n', 'J', 'mzJ`z')
+
+-- Keep screen centered while jumping up and down
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+
+-- Keep screen centered while searching
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzzv')
+
+-- Delete highlighted word and add to void register (paste without yanking)
+vim.keymap.set('x', '<leader>p', '"_dP')
+
+-- yank into system clipboard
+vim.keymap.set('n', '<leader>y', '"+y')
+vim.keymap.set('v', '<leader>y', '"+y')
+vim.keymap.set('n', '<leader>Y', '"+y')
+
+-- Disable Q hotkey
+vim.keymap.set('n', 'Q', '<nop>')
+
+-- Quickfix navigation to find errors or something lol
+vim.keymap.set('n', '<C-k>', '<cmd>cnext<CR>zz')
+vim.keymap.set('n', '<C-j>', '<cmd>cprev<CR>zz')
+vim.keymap.set('n', '<leader>k', '<cmd>lnext<CR>zz')
+vim.keymap.set('n', '<leader>j', '<cmd>lprev<CR>zz')
+
+-- Find and replace realtime
+vim.keymap.set('n', '<leader>rr', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- Double space :so
+vim.keymap.set('n', '<leader><leader>', function()
+  vim.cmd 'so'
+end)
+
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
