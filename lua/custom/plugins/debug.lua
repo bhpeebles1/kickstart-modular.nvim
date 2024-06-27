2efdd8267dffa6b35dcd907288db3033559eb188
blakebluearrow <blake@bluearrowmail.com>
2024-06-27 10:08:02 -0400

    Added python config

--- lua/custom/plugins/debug.lua
+++ lua/custom/plugins/debug.lua
@@ -117,5 +117,25 @@ return {
         },
       },
     }
+
+    dap.adapters.python = {
+      type = 'executable',
+      command = 'python',
+      args = { '-m', 'debugpy.adapter' },
+    }
+
+    dap.configurations.python = {
+      {
+        type = 'python',
+        request = 'launch',
+        name = 'Launch file',
+        program = '${file}',
+        pythonPath = function()
+          return 'python'
+          -- return 'C:\\Users\\blake\\AppData\\Local\\Programs\\Python\\Python312'
+        end,
+      },
+    }
+
   end,
 }
