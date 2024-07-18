03809bf1ff9b2898f5f7d0ffc5c45c6d81c00065
blakebluearrow <blake@bluearrowmail.com>
2024-07-18 09:09:27 -0400

    moved hotkey to hotkeys section

--- lua/custom/plugins/debug.lua
+++ lua/custom/plugins/debug.lua
@@ -26,6 +26,7 @@ return {
     vim.keymap.set('n', '<F7>', dap.step_into, { desc = 'Debug: Step Into' })
     vim.keymap.set('n', '<F8>', dap.step_over, { desc = 'Debug: Step Over' })
     vim.keymap.set('n', '<F9>', dap.step_out, { desc = 'Debug: Step Out' })
+    vim.keymap.set('n', '<F10>', dapui.toggle, { desc = 'Debug: See last session result.' })
     vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
     vim.keymap.set('n', '<leader>B', function()
       dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
@@ -78,7 +79,6 @@ return {
       windows = { indent = 1 },
     }
 
-    vim.keymap.set('n', '<F10>', dapui.toggle, { desc = 'Debug: See last session result.' })
 
     dap.listeners.after.event_initialized['dapui_config'] = dapui.open
     dap.listeners.before.event_terminated['dapui_config'] = dapui.close
