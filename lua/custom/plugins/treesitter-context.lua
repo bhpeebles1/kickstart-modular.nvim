c2d577b295cb86ae53a4f40bd367cc0f764c72a9
blakebluearrow <blake@bluearrowmail.com>
2024-08-07 16:51:16 -0400

    might use treesitter context someday, but today is not that day

---
+++ lua/custom/plugins/treesitter-context.lua
@@ -0,0 +1,18 @@
+return {
+  --[[ {
+    'nvim-treesitter/nvim-treesitter-context',
+    config = function()
+      require'treesitter-context'.setup{
+        enable = true,
+        throttle = true,
+        max_lines = 0,
+        patterns = {
+          default = {
+            'class', 'function', 'method',
+          },
+        },
+      }
+    end,
+  }, ]]
+}
+
